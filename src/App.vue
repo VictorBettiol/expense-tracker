704ff7a4c04fd26c9221ead76d2614b5aad4e51e
Victor Bettiol <victor.bettiol@hotmail.com>
2024-02-14 03:54:44 +0000

    feat!: add transaction, toast notifications, added uuid and vue-toastification

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total" />
-    <IncomeExpenses :income="income" :expenses="expenses" />
+    <Balance :total="+total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
   </div>
 </template>
 
 <script setup>
+import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+import { ref, computed, toDisplayString } from "vue";
+import { useToast } from "vue-toastification";
 
-import { ref, computed } from "vue";
+const toast = useToast();
 
 const transactions = ref([
   { id: 1, text: "Flower", amount: -19.99 },
@@ -50,4 +53,14 @@ const expenses = computed(() => {
     }, 0)
     .toFixed(2);
 });
+
+// Add transaction
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: uuidv4(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+  toast.success("Transaction added");
+};
 </script>
